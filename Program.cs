fcab11c32b358b7c3985df4f5419165502ec55bc
Gazi Emrah <g.emrahartac@gmail.com>
2021-12-30 14:27:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ad = "asyam";
+            Console.WriteLine(ad);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
